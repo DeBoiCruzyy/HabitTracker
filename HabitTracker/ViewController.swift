407c315d3e95003fc0cde4693e839eecbf632aaf
DeBoiCruzyy <cruz.arreola.heat@gmail.com>
2025-09-24 01:23:55 -0700

    Got table to appear, but the date is chopped off

--- HabitTracker/ViewController.swift
+++ HabitTracker/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
-    @IBOutlet weak var table: UITableView!      // Interface builder
-
+    @IBOutlet weak var tableView: UITableView!      // IB = Interface builder
+    
+    @IBOutlet weak var habitTextField: UITextField!
+    
+    var habits: [(habit: String, date: String)] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // may need to come back here
+    }
+    
+    @IBAction func addHabit(_ sender: UIButton) {
+        let habitText = habitTextField.text!        //pulls text from text field
+        
+        let formatter = DateFormatter()             // This part pulls current date/time
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        let currentDate = formatter.string(from: Date())
+        
+        habits.append((habit: habitText, date: currentDate))    /* adds habit and date/time
+                                                                 to table */
+        
+        tableView.reloadData()      // refreshes table
+        
+        habitTextField.text = ""
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return habits.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HabitCell", for: indexPath) as! HabitCell
+        let habitEntry = habits[indexPath.row]
+        
+        cell.habitLabel.text = habitEntry.habit
+        cell.dateLabel.text = habitEntry.date
+        
+        return cell
     }
 
 
